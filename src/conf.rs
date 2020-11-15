bd66a6cca6fb43673e0eaaeaa331084f144d2ae9
jaq-cat <jaq.cat@protonmail.ch>
2020-11-15 16:11:17 -0300

    rand stuff

--- src/conf.rs
+++ src/conf.rs
@@ -1,5 +1,15 @@
 pub const DIM: u32 = 500;
+
+// ship
 pub const COOLDOWN: u8 = 15;
 pub const MAXSPEED: f64 = 100.0;
 pub const SHIP_WIDTH: f64 = 5.0;
 pub const SHIP_HEIGHT: f64 = 15.0;
+
+// asteroids
+pub const ASTSPD: f64 = 1.0;
+pub const AST_RAD: f64 = 40.0;
+pub const AST_SIZE_VAR: f64 = 0.4;
+pub const AST_EDGES: u8 = 25;
+pub const AST_COUNT: usize = 7;
+pub const AST_ROUND: f64 = 0.7;
---
+++ src/randranges.rs
@@ -0,0 +1,30 @@
+use rand::{distributions::Uniform, rngs::ThreadRng, thread_rng};
+
+use super::conf::*;
+
+pub struct Ranges {
+    pub rng: ThreadRng,
+    pub zero_one: Uniform<u8>,
+
+    pub dim_half: Uniform<f64>,
+
+    pub ast_edges: Uniform<u8>,
+    pub ast_r: Uniform<f64>,
+    pub ast_speed: Uniform<f64>,
+
+    pub p_speed: Uniform<f64>,
+}
+
+impl Ranges {
+    pub fn new() -> Self {
+        Self {
+            rng: thread_rng(),
+            zero_one: Uniform::from(0..=1),
+            dim_half: Uniform::from(0.0..=DIM as f64),
+            ast_edges: Uniform::from(AST_EDGES / 2..=AST_EDGES),
+            ast_r: Uniform::from(AST_RAD * AST_SIZE_VAR..=AST_RAD),
+            ast_speed: Uniform::from(ASTSPD * 0.5..=ASTSPD),
+            p_speed: Uniform::from(-1.0..=1.0),
+        }
+    }
+}
