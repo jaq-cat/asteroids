f3e50440331cb540ce3a8e910dbbe20de215e45c
jaq-cat <jaq.cat@protonmail.ch>
2020-11-15 15:38:52 -0300

    translation :D

--- src/render.rs
+++ src/render.rs
@@ -5,6 +5,12 @@ pub fn render(state: &State, c: &Context, g: &mut G2d) {
     for i in (0..state.ship.shape.len() - 1).step_by(2) {
         let (x1, y1) = state.ship.shape[i];
         let (x2, y2) = state.ship.shape[i + 1];
-        line([1.0; 4], 1.0, [x1, y1, x2, y2], c.transform, g);
+        line(
+            [1.0; 4],
+            1.0,
+            [x1, y1, x2, y2],
+            c.transform.trans(state.ship.x, state.ship.y),
+            g,
+        );
     }
 }
